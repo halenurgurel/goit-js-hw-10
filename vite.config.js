df5015fa861ff9332be6d6ec43cdb13e2aafe659
Halenur Gurel <halenur.gurel@hotmail.com>
2025-12-24 13:30:57 +0300

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import glob from 'glob';
+import { resolve } from 'path';
+import vitePluginFullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
+  base: '/goit-js-hw-10/',
   server: {
     port: 3000,
     open: true,
   },
+  build: {
+    rollupOptions: {
+      input: glob.sync('./*.html'),
+    },
+    outDir: 'dist',
+  },
+  plugins: [vitePluginFullReload(['./src/**/**.js', './*.html'])],
 });
